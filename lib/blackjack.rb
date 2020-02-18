f8d3151873c3937d71d42ecff7298e29c93bcaae
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-18 14:54:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,11 +45,11 @@ def hit?(total_hand)
   prompt_user
   total_hand = deal_card + deal_card 
   input = get_user_input
-  if input == "s"
-    total_hand
-  elsif 
-    input == "h"
+  if input == "h"
     total_hand += deal_card
+  elsif 
+    input == "s"
+    total_hand
   else
     invalid_command
   end
