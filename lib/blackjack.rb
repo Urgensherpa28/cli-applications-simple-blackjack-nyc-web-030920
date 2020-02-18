1fdfdee12609867a49c6087e6ac1cad31c808b31
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-18 14:21:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,38 +1,61 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  random = rand(1..11)
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  input = gets.chomp
 end
 
-def end_game
+def end_game(total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  hand1 = deal_card
+  hand2 = deal_card 
+  total_hand = hand1 + hand2 
+  display_card_total(total_hand)
+  return total_hand
 end
 
-def hit?
+def hit?(total_hand)
   # code hit? here
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    total_hand += deal_card
+  elsif  
+    input == "s"
+    total_hand
+  else 
+    invalid_command
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
+
 
 #####################################################
 # get every test to pass before coding runner below #
